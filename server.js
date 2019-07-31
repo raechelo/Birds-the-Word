7ae79bfa3e47f19393160d20ebdb49bc43b8b32e
raechel <odom.raechel@gmail.com>
2019-07-31 11:35:25 -0600

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
-const app = express();
+const path = require('path');
+const serveStatic = require('serve-static')
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, (req, res) => {
-  res.sendfile(__dirname + '/dist/index.html');
-});
+const app = express();
+// app.use(express.static(__dirname + '/dist/'));
+// app.get(/.*/, (req, res) => {
+//   res.sendfile(__dirname + '/dist/index.html');
+// });
+
+app.use('/', serveStatic(path.join(__dirname, '/dist/')))
 
 app.listen(port);
 
